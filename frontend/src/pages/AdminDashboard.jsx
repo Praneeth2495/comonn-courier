212ea7342ae2e725f3d3d4bcd538005519b1319a
Praneeth <Praneeth@MacBook-Air-2.local>
2026-08-13 20:04:11 +1000

    Auto-commit: AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -522,7 +522,7 @@ function OrdersPanel() {
   }
   useEffect(() => {
     load();
-    const interval = setInterval(load, 3 * 60 * 1000);
+    const interval = setInterval(() => load(true), 60 * 1000);
     return () => clearInterval(interval);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tab, page, pageSize, originState, originRegion, deliveryCountry, deliverySelectedAirportCodes.join(',')]);
