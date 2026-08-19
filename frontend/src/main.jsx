d146248d1ecad802ce2cb26b6ca2138d82f84c24
Praneeth <Praneeth@MacBook-Air-2.local>
2026-08-20 08:27:13 +1000

    Auto-commit: main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initNativeApp } from './nativeApp';
 import './styles/theme.css';
+
+initNativeApp();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
