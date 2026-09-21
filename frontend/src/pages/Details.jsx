d9d8bba8ba6ff63077962807d070a8c57b186b98
Praneeth2495 <169018688+Praneeth2495@users.noreply.github.com>
2026-09-21 14:32:53 +1000

    Auto-commit: Details.jsx

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -347,7 +347,7 @@ export default function Details() {
               hideCityStatePlaceholder
               savedAddresses={receiverSavedAddresses}
               onSelectSaved={pickSavedReceiverAddress}
-              lockedFields={receiverLocked ? ['postcode', 'city', 'state', 'countryCode'] : []}
+              lockedFields={receiverLocked ? ['postcode', ...(receiverCityLocked ? ['city'] : []), 'state', 'countryCode'] : []}
             />
           </div>
 
