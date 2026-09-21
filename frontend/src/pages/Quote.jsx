459e39d0475715eb9c1ab1bc3b888815871249b9
Praneeth2495 <169018688+Praneeth2495@users.noreply.github.com>
2026-09-21 14:31:02 +1000

    Auto-commit: Quote.jsx

--- frontend/src/pages/Quote.jsx
+++ frontend/src/pages/Quote.jsx
@@ -488,7 +488,7 @@ export default function Quote() {
                 <div className="card" style={{ position: 'absolute', top: '100%', left: 0, right: 0, marginTop: 4, padding: 6, maxHeight: 220, overflowY: 'auto', zIndex: 20 }}>
                   {originSuggestions.map((s, i) => (
                     <button type="button" key={i} className="acct-menu-item" onClick={() => pickOriginSuggestion(s)}>
-                      {originPostcode}, {s.suburb}{suburbStateSuffix(s.suburb, s.state)}
+                      {formatPostcodeSuggestion(originPostcode, s.suburb, s.state)}
                     </button>
                   ))}
                 </div>
