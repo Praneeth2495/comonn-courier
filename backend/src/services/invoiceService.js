7848c1ba6264176fe33e82dee44e38aeff88e7a3
Praneeth2495 <169018688+Praneeth2495@users.noreply.github.com>
2026-09-22 20:27:17 +1000

    Auto-commit: invoiceService.js

--- backend/src/services/invoiceService.js
+++ backend/src/services/invoiceService.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 const { getCountryName } = require('../utils/countryNames');
+const { sanitizePdfText } = require('../utils/pdfText');
 
 const STORAGE_DIR = process.env.LABEL_STORAGE_DIR || path.join(__dirname, '../../storage/labels');
 const LOGO_PATH = path.join(__dirname, '../assets/logo-full.png');
